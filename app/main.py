3b6599ce2d48fe14a2c8f10e6fae96026e776aab
akash-grayfrost <akash@grayfrost.app>
2025-12-07 19:01:51 +0530

    requirements

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-# app/main.py
+"""
+app/main.py
+"""
 
 import argparse
 from pathlib import Path
@@ -12,14 +14,19 @@ SETTINGS_PATH_DUMMY = BASE_DIR / "config" / "settings_dummy.yaml"
 SETTINGS_PATH_LIVE = BASE_DIR / "config" / "settings_live.yaml"
 SETTINGS_PATH_BACKTEST = BASE_DIR / "config" / "settings_backtest.yaml"
 
-
 def parse_args():
+    """
+    Parse command line arguments
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", choices=["dummy", "backtest", "live"], default="dummy")
     return parser.parse_args()
 
 
 def main():
+    """
+    Main function
+    """
     args = parse_args()
 
     # Load config dynamically
